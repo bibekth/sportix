c8707b1726b8de620cf2c281068ffd3f4e504716
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-08-23 18:57:17 +0000

    login register

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['namespace' => 'App/Http/Controllers/API'], function () {
+    Route::group(['controller' => 'AuthController'], function() {
+        Route::post('login', 'login')->name('login');
+        Route::post('register', 'register')->name('register');
+    });
+});
