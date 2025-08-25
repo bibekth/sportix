70263bd986ab8ad1a3ce806bc5047b70c75a522f
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-08-25 17:51:27 +0000

    changes

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -8,9 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex">
-                {{-- Sidebar --}}
-                @include('layouts.sidenav')
-
                 {{-- Main Content Area --}}
                 <div class="flex-1 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
